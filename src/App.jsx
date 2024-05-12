2b2f1c7ac59047ad25874793282a16ec9c27e752
Herlan <herlanherbas@gmail.com>
2024-05-11 21:22:51 -0400

    feature(added tailwindCSS)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +24,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <p className='text-cyan-400 font-bold'>HOLA MUNDO</p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
