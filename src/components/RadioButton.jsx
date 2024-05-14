9dc1c921583baab58354f7a2985685cfdf4e0637
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-05-13 21:32:29 -0400

    update component Shares

---
+++ src/components/RadioButton.jsx
@@ -0,0 +1,15 @@
+const RadioButton = ({ id, name, label }) => {
+    return (
+      <div>
+        <input
+          type="radio"
+          name={name}
+          id={id}
+          className="mr-2 h-4 w-4 text-lg"
+        />
+        <label htmlFor={id}>{label}</label>
+      </div>
+    );
+  };
+  
+  export default RadioButton;
