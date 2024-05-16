6c8d2dbb844dffffa2e972d7f19d16581418152e
AndyOrtz <andy.ortiz.f21@gmail.com>
2024-05-16 18:09:42 -0400

    added TextInput component

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
+import search from "../../pages/Shares/assets/search.svg";
 import RadioButton from "../../components/RadioButton/RadioButton.jsx";
-
+import TextInput from "../../components/TextInput/TextInput.jsx";
 export default function StartUp() {
   return (
     <>
@@ -9,15 +10,23 @@ export default function StartUp() {
           <h1 className="text-xl font-bold font-secular">New Share</h1>
           <hr className="bg-black border-none h-0.5 ml-5 rounded grow"/>
         </div>
-        <section className="mx-60 px-56">
+        <section className="mx-54 px-56 space-y-1">
           <h3 className="">Identification</h3>
-            <section className="">
-                <label>Share Type</label>
-                <div className="px-5">
-                    <RadioButton id= "optprint" name = "shrtyp" label = "Printer"/>
-                    <RadioButton id= "optdirec" name = "shrtyp" label = "Directory"/>
-                </div>
-        </section>
+            <TextInput label="Share Name"/>
+            <TextInput label="Share Description"/>
+            <section className="flex">
+              <label>Share Type</label>
+              <div className="px-5">
+                <RadioButton id= "optprint" name = "shrtyp" label = "Printer"/>
+                <RadioButton id= "optdirec" name = "shrtyp" label = "Directory"/>
+              </div>
+            </section>
+            <div className="flex space-x-2">
+              <TextInput label="Share Path"/>
+              <button className="bg-customHover items-center justify-center flex rounded w-8 h-8">
+                <img src={search} alt="Filtro" className="w-6 h-6 opacity-none"/>
+              </button>
+            </div>
         </section>
       </section>
     </>
