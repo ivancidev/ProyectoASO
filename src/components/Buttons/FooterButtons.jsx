a62df147b57691ff0b04ef65b0109bf99e73596f
Herlan <herlanherbas@gmail.com>
2024-05-23 13:53:23 -0400

    update: FooterButton

--- src/components/Buttons/FooterButtons.jsx
+++ src/components/Buttons/FooterButtons.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Help from "../../modals/Help";
 import Succesfully from "../../modals/Succesfully";
 import { fetchRename } from "../../utils/api";
