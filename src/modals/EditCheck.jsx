976f8e7c2cf73b868cd3ea9aa416e4c1a50a8d85
Herlan <herlanherbas@gmail.com>
2024-05-24 18:22:17 -0400

    update: EditCheck

--- src/modals/EditCheck.jsx
+++ src/modals/EditCheck.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
 import close from "../assets/close.svg";
+import { useState } from "react";
+import { fectchEdit } from "../utils/api";
 
-const EditCheck = ({ isOpen, onClose, name, updateShareName, option }) => {
-  const [newValue, setNewValue] = useState(name);
-
-  const handleChange = (e) => {
-    setNewValue(e.target.value);
-  };
-
-  const handleUpdate = () => {
-    updateShareName(newValue);
-    onClose();
-  };
-
+const EditCheck = ({ isOpen, onClose, selectedValue, selectedKey, name }) => {
   if (!isOpen) return null;
+  const [isChecked, setIsChecked] = useState(selectedValue === 'Yes');
+
+  const handleOnChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleConfirm = () => {
+    const jsonData = JSON.stringify({ [selectedKey]: isChecked? 'Yes':'No' });
+    console.log('es el', jsonData)
+    fectchEdit(name, jsonData);
+    onClose();
+    window.location.href = "/Shares";
+  }
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-slate-400 bg-opacity-[0.4] z-50">
@@ -24,30 +27,29 @@ const EditCheck = ({ isOpen, onClose, name, updateShareName, option }) => {
         >
           <img src={close} alt="close" />
         </button>
-        <div className="flex flex-col">
-          <p className="font-secular text-base">Current Option: <b>{option}</b> </p>
-          <p className="font-roboto text-base ml-2">{option}</p>
-          <input
-            type="text"
-            value={newValue}
-            onChange={handleChange}
-            className="border-2 border-black rounded-lg p-2 mt-4 w-full"
-          />
+        <div className="flex flex-row">
+            <p className="font-secular text-base">Current Option:</p>
+            <p className="font-roboto text-base ml-2">{selectedKey}</p>
         </div>
-        
-        <div className="flex items-center justify-center space-x-6 mt-8">
-          <button
-            onClick={handleUpdate}
-            className="w-20 py-2 bg-customHover rounded-[20px] text-white"
-          >
-            Ok
-          </button>
-          <button
-            onClick={onClose}
-            className="w-20 py-2 border-black border-[1px] text-customHover rounded-[20px]"
-          >
-            Cancel
-          </button>
+        <div className="mt-4 flex items-center">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleOnChange}
+            className="mr-2"
+          />
+          <label htmlFor="isChecked" className="font-roboto text-base">
+            {selectedKey}
+          </label>
+        </div>
+        <div className="flex items-center justify-center mt-8 space-x-6">
+            <button onClick={handleConfirm}
+             className="w-20 py-2 bg-customHover rounded-[20px] text-white">
+               Guardar 
+            </button>
+            <button onClick={onClose}  className="w-20 py-2 border-black border-[1px] text-customHover rounded-[20px]">
+                Cancel
+            </button>
         </div>
       </div>
     </div>
