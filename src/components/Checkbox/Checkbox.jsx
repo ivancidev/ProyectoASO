6372bc98a0528e78204af04a50088ecf1d550d4c
Herlan <herlanherbas@gmail.com>
2024-05-25 12:41:51 -0400

    add: props

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,21 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-const Checkbox = ({ label, name, checked = false, onChange }) => {
-  const [isChecked, setIsChecked] = useState(checked); // Set default checked state
+const Checkbox = ({
+  label,
+  name,
+  checked = false,
+  onChange,
+  disabled = false,
+}) => {
+  const [isChecked, setIsChecked] = useState(checked);
+
+  useEffect(() => {
+    setIsChecked(checked);
+  }, [checked]);
 
   const handleChange = (event) => {
+    const value = event.target.checked ? "Yes" : "No";
     setIsChecked(event.target.checked);
-    onChange(event.target.checked);
+    onChange(name, value);
   };
 
   return (
     <div className="space-x-3 flex">
-      <input className='w-4'
+      <input
+        className="w-4"
         type="checkbox"
         id={name}
         name={name}
         checked={isChecked}
         onChange={handleChange}
+        disabled={disabled}
       />
       <label htmlFor={name}>{label}</label>
     </div>
