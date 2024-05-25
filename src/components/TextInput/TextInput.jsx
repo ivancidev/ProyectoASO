d70cfe6060fb410dcccf81ffa7e467bdded9804e
Herlan <herlanherbas@gmail.com>
2024-05-25 12:41:24 -0400

    add: props

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,20 +1,14 @@
-import { useState } from "react";
 
-const TextInput = ({ label }) => {
-  const [inputValue, setInputValue] = useState(null);
-
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
+const TextInput = ({ label, value, onChange, disabled }) => {
   return (
     <div className="space-x-6 ml-6">
       <label>{label}</label>
       <input
         type="text"
-        value={inputValue}
-        onChange={handleChange}
-        className="border-[1px] border-zinc-500 rounded-lg p-1 focus:outline-none"
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        className={`border-[1px] border-zinc-500 rounded-lg p-1 focus:outline-none ${disabled ? "bg-gray-200" : ""}`}
       />
     </div>
   );
