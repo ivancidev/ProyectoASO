3cd9e0d080f595b8154f6d6fc26592a652623328
Herlan <herlanherbas@gmail.com>
2024-05-25 12:42:08 -0400

    add: props

--- src/components/Buttons/FooterButtonAdd.jsx
+++ src/components/Buttons/FooterButtonAdd.jsx
@@ -2,27 +2,17 @@ import React, { useState } from "react";
 import Help from "../../modals/Help";
 import Succesfully from "../../modals/Succesfully";
 import { fetchRename } from "../../utils/api";
-import Button from "./Button";
-const FooterButtonAdd = ({
-  title,
-  description,
-  oldName,
-  newName,
-  setNewName,
-}) => {
+const FooterButtonAdd = ({ title, description, handleSubmit, name }) => {
   const [isModalHelp, setModalHelp] = useState(false);
   const [showMessage, setShowMessage] = useState(false);
 
-  const handleClick = () => {
-    if (newName !== null) {
-      fetchRename(oldName, newName);
-      setShowMessage(true);
-    }
+  const handleSend = () => {
+    handleSubmit();
+    setShowMessage(true);
   };
 
   const handleLoadingComplete = () => {
     setShowMessage(false);
-    setNewName(null);
   };
   return (
     <>
@@ -38,7 +28,7 @@ const FooterButtonAdd = ({
       <Succesfully
         show={showMessage}
         onLoadingComplete={handleLoadingComplete}
-        newName={newName}
+        newName={name}
       />
 
       <div className="flex items-center justify-between font-roboto text-sm bottom-0 left-0 right-0 p-6 fixed">
@@ -49,18 +39,18 @@ const FooterButtonAdd = ({
           ?
         </button>
         <div className="flex items-center justify-between w-64">
-          <button
-            
-            className="hover:border-curtomButton w-28 h-10 p-1 text-customHover rounded-[100px] flex items-center justify-center border-[1px] border-customBlack"
-          >
-            <p>Back</p>
-          </button>
-          <button
-            onClick={handleClick}
-            className="hover:bg-curtomButton bg-customHover w-28 h-10 p-1 text-white rounded-[100px] flex items-center justify-center"
-          >
-            <p>Continue</p>
-          </button>
+          {name ? (
+            <button
+              onClick={handleSend}
+              className="hover:border-curtomButton w-28 h-10 p-1 text-customHover rounded-[100px] flex items-center justify-center border-[1px] border-customBlack"
+            >
+              <p>Guardar</p>
+            </button>
+          ) : (
+            <button className="hover:bg-curtomButton bg-customHover w-28 h-10 p-1 text-white rounded-[100px] flex items-center justify-center">
+              <p>Ok</p>
+            </button>
+          )}
         </div>
       </div>
     </>
